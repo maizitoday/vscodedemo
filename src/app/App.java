4e8d428f58cdd4af15765f0eb63a7d191e4a1f5f
maizitoday <maizi_today@sina.com>
2019-05-05 22:52:15 +0800

    yb2,我是分支2号

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java+++");
+        System.out.println("我是分支2号+++");
     }
 }
