6cbd2bb4f4a06e7501f6bae4e60bd18b95189f26
maizitoday <maizi_today@sina.com>
2019-05-05 22:47:56 +0800

    分支1号

--- src/app/App.java
+++ src/app/App.java
@@ -2,7 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("哈哈，我是yb分支");
+        System.out.println("哈哈，我是yb分支---------");
         
     }
 }
